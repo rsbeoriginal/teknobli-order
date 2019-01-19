008be32f834b2fff01d9647f09ef9cf7e9d31227
rishabhkochar <rishabhkochar58@gmail.com>
2019-01-19 22:58:16 +0530

    Initial Commit

---
+++ order/src/main/java/com/tecknobli/order/OrderApplication.java
@@ -0,0 +1,14 @@
+package com.tecknobli.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
+
