b689d79a519e17e3be8548a2fbb3cf684e8a6cef
rishabhkochar <rishabhkochar58@gmail.com>
2019-01-20 19:16:35 +0530

    TEst 1

--- order/src/main/java/com/tecknobli/order/controller/UserOrderController.java
+++ order/src/main/java/com/tecknobli/order/controller/UserOrderController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,7 +21,7 @@ public class UserOrderController {
     UserOrderService userOrderService;
 
     @RequestMapping(value = "/add",method = RequestMethod.POST)
-    public ResponseEntity<UserOrder> addOrder(UserOrderDTO userOrderDTO){
+    public ResponseEntity<UserOrder> addOrder(@RequestBody UserOrderDTO userOrderDTO){
         UserOrder userOrder = new UserOrder();
         BeanUtils.copyProperties(userOrderDTO, userOrder);
         UserOrder orderCreated = userOrderService.save(userOrder);
