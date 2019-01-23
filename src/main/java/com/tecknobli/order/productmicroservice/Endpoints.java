916818b3d1535bba9797924fae3de544ff0b6399
rishabhkochar <rishabhkochar58@gmail.com>
2019-01-23 21:45:21 +0530

    Endpoints for heroku

--- src/main/java/com/tecknobli/order/productmicroservice/Endpoints.java
+++ src/main/java/com/tecknobli/order/productmicroservice/Endpoints.java
@@ -2,7 +2,7 @@ package com.tecknobli.order.productmicroservice;
 
 public class Endpoints {
 
-    public static final String BASE_URL="http://localhost:8002/product/";
+    public static final String BASE_URL="https://teknobli-product.herokuapp.com/product/";
 
     public static final String SINGLRPRODUCT_URL = "/select/";
     public static final String MERCHANT_BASE_URL = "http://localhost:8080/";
--- src/main/java/com/tecknobli/order/reviewmicroservices/EndPoints.java
+++ src/main/java/com/tecknobli/order/reviewmicroservices/EndPoints.java
@@ -2,7 +2,7 @@ package com.tecknobli.order.reviewmicroservices;
 
 public class EndPoints {
 
-    public static final String BASE_URL="http://localhost:8000";
+    public static final String BASE_URL="https://teknobli-review.herokuapp.com";
     public static final String GET_USER_PRODUCT_RATING = "/product/getUserRating";
     public static final String GET_USER_MERCHANT_RATING = "/merchant/getUserRating";
 }
